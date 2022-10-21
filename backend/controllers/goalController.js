904aff637a2dea095469c38fccb2500533d8836c
chechna9 <ja_bouchouareb@esi.dz>
2022-10-21 21:50:15 +0100

    link mongo db

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -1,32 +1,35 @@
+const asyncHandler = require('express-async-handler')
+
+
 // @desc Get goals
 // @route GET /api/goals
 // @access Private
-const getGoals = (req, res) => {
+const getGoals = asyncHandler(async(req, res) => {
   res.status(200).json({ message: "Get goals" });
-};
+});
 // @desc Set goal
 // @route Set /api/goals
 // @access Private
-const setGoal = (req, res) => {
+const setGoal = asyncHandler (async(req, res) => {
   if (!req.body.text) {
     res.status(400);
     throw new Error("Please add text field");
   }
 
   res.status(200).json({ message: "set goal" });
-};
+});
 // @desc Update goals
 // @route Put /api/goals/:id
 // @access Private
-const updateGoal = (req, res) => {
+const updateGoal = asyncHandler(async(req, res) => {
   res.status(200).json({ message: `update goal ${req.params.id}` });
-};
+});
 // @desc Delete goals
 // @route Delete /api/goals/:id
 // @access Private
-const deleteGoal = (req, res, id) => {
+const deleteGoal = asyncHandler(async(req, res, id) => {
   res.status(200).json({ message: `delete goal ${req.params.id}` });
-};
+});
 
 module.exports = {
   getGoals,
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,12 @@
 const express = require("express");
+const colors = require('colors');
 const dotenv = require("dotenv").config();
+const connectDB = require('./config/db');
 const port = process.env.PORT || 5000;
 const {errorHandler} = require('./middleware/errorMiddleware');
+
+connectDB();
+ 
 const app = express();
 
 app.use(express.json())//middle ware

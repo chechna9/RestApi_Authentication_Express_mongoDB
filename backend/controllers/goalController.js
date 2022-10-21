0be0b9c96c326a5aa0d29ae85239e4154161c900
chechna9 <ja_bouchouareb@esi.dz>
2022-10-21 20:05:00 +0100

    add error handler

--- backend/controllers/goalController.js
+++ backend/controllers/goalController.js
@@ -7,24 +7,30 @@ const getGoals = (req, res) => {
 // @desc Set goal
 // @route Set /api/goals
 // @access Private
-const setGoal = (req,res)=>{
-    res.status(200).json({ message: "set goal" });
-}
+const setGoal = (req, res) => {
+  if (!req.body.text) {
+    res.status(400);
+    throw new Error("Please add text field");
+  }
+
+  res.status(200).json({ message: "set goal" });
+};
 // @desc Update goals
 // @route Put /api/goals/:id
 // @access Private
-const updateGoal = (req,res)=>{
-    res.status(200).json({ message: `update goal ${req.params.id}` });
-}
+const updateGoal = (req, res) => {
+  res.status(200).json({ message: `update goal ${req.params.id}` });
+};
 // @desc Delete goals
 // @route Delete /api/goals/:id
 // @access Private
-const deleteGoal = (req,res,id)=>{
-    res.status(200).json({ message: `delete goal ${req.params.id}` });
-}
+const deleteGoal = (req, res, id) => {
+  res.status(200).json({ message: `delete goal ${req.params.id}` });
+};
 
 module.exports = {
   getGoals,
   setGoal,
-  updateGoal,deleteGoal
+  updateGoal,
+  deleteGoal,
 };
--- backend/server.js
+++ backend/server.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const dotenv = require("dotenv").config();
 const port = process.env.PORT || 5000;
-
+const {errorHandler} = require('./middleware/errorMiddleware');
 const app = express();
 
+app.use(express.json())//middle ware
+app.use(express.urlencoded({extended:false}))
+
 app.use("/api/goals", require("./routes/goalRoutes"));
+app.use(errorHandler);
 
 app.listen(port, () => console.log(`server started on port ${port}`));

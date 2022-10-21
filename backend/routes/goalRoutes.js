29cf44d1fc5275523f9939594165da23e2946344
chechna9 <ja_bouchouareb@esi.dz>
2022-10-21 14:48:21 +0100

    init 4 crud routes

---
+++ backend/routes/goalRoutes.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+router.get("/", (req, res) => {
+  res.status(200).json({ message: "Get goals" });
+});
+router.post("/", (req, res) => {
+  res.status(200).json({ message: "set goal" });
+});
+router.put("/:id", (req, res) => {
+  res.status(200).json({ message: `update goal ${req.params.id}` });
+});
+router.delete("/:id", (req, res) => {
+  res.status(200).json({ message:`delete goal ${req.params.id}` });
+});
+
+module.exports = router;
